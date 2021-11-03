17a9a80ec4e8512f046f92fca5db0fd81cd8ef6d
Joe <66766658+ghost9425@users.noreply.github.com>
2021-11-03 15:11:48 +0700

    edit

--- layouts/nav.php
+++ layouts/nav.php
@@ -76,10 +76,6 @@
                                         <span class="sv-normal" set-lan="text:3.5.4">3.10
                                         Update Password</span></a>
                                 </li>
-                                <li id="manu311"><a class="waves-effect submenu" href="apitransferwallet.php#tabEleven">
-                                        <span class="sv-normal" set-lan="text:3.5.4">3.11
-                                        Update Password No-Address</span></a>
-                                </li>
                             </ul>
                         </div>
                     </li>
